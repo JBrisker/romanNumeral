84eb769e8e7b7a96ecfc3a07569b03fb13251039
Jayna Brisker <briskerjayna@gmail.com>
2026-05-06 11:23:37 -0700

    Reformatted code and applied exceptions.

--- romanNumerals.py
+++ romanNumerals.py
@@ -2,42 +2,51 @@
 
 
 numeral=''
-try:
+while True:
     value=input("Enter a value 1-10: ")
-    if value.isnumeric():
-        value =int(value)
-except Exception:
-    if not value.isnumeric():
-       print("ERROR ENTER A NUMBER")
-    value=input("Enter a value 1-10 ")
-
-     
-else: 
-    while value < 1 or value > 10:
-        print("The range is 1-10")
-        value=int(input("Enter a number 1-10 "))
-
-    if value == 10:
-     numeral += "X"
-
-    elif value == 9:
-        numeral = "IX"
-
-    elif value % 5 == 0:
-        numeral += "V"
-
-    elif value > 5:
-     numeral += "V"
-     for num in range(value - 5):
-        numeral += "I"
-
-    elif value == 4:
-        numeral ="IV"
+    if not value =='':
+        try:
+                value.isnumeric()
+                value = int(value)
+                if value < 1 or value > 10:
+                    print("ERROR: Value out of range ")
+                    continue
+                else:
+                    break
+        
+        except Exception:
+         if not value.isnumeric():
+            print("ERROR: Enter a number")
+            continue
     else:
-     val = value
-     while val > 0:
-        numeral += "I"
-        val -= 1 
+          print("Please enter a value")
+          continue
+
+if value == 10:
+         numeral += "X"
+        
+
+elif value == 9:
+         numeral = "IX"
+        
+elif value > 5 and value < 9:
+        numeral += "V"
+        for num in range(value - 5):
+            numeral += "I"
+
+elif value % 5 == 0:
+            numeral += "V"           
+
+elif value == 4:
+         numeral ="IV"
+         
+
+elif value < 4 :
+            val = value
+            while val > 0:
+                numeral += "I"
+                val -= 1 
+              
 
 print(f"the roman numeral of {value} is: {numeral}")
     
